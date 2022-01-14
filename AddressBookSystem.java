e18ae898f171560e3284570594f3c29d8648a8ac
pranjali khadatkar <khadatkarpranjali1994@gmail.com>
2022-01-15 00:59:00 +0530

    Added: DeleteContact

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -23,60 +23,16 @@ public class AddressBookSystem {
 	
 public void setPersonDetails() 
 {
-
-	Person person1 = new Person("Pranjali", "khadatkar", "besa", "Nagpur", "Maharastra", "India", "8421078744","440034", "pranjalikhadatkar@gmail.com");
-	personList.add(person1);
-	Person person2 = new Person("pranay", "khadatkar", "besa", "Nagpur", "Maharastra", "India", "9012197036","440027", "khadatkarpranay@gmail.com");
-	personList.add(person2);
-	Person person3 = new Person("vishal", "pothare", "Pimpalgaon", "Hinganghat", "Maharastra", "India", "9637416031","442301", "potharevishal@gmail.com");
-	personList.add(person3);
-	Person person4 = new Person("kajal", "palange", "wardha", "wardha", "Maharastra", "India", "9430254160","440021", "kajalpalange@gmail.com");
-	personList.add(person4);
+		Person person1 = new Person("Pranjali", "khadatkar", "besa", "Nagpur", "Maharastra", "India", "8421078744","440034", "pranjalikhadatkar@gmail.com");
+		personList.add(person1);
+		Person person2 = new Person("pranay", "khadatkar", "besa", "Nagpur", "Maharastra", "India", "9012197036","440027", "khadatkarpranay@gmail.com");
+		personList.add(person2);
+		Person person3 = new Person("vishal", "pothare", "Pimpalgaon", "Hinganghat", "Maharastra", "India", "9637416031","442301", "potharevishal@gmail.com");
+		personList.add(person3);
+		Person person4 = new Person("kajal", "palange", "wardha", "wardha", "Maharastra", "India", "9430254160","440021", "kajalpalange@gmail.com");
+		personList.add(person4);
 }
-public Person createcontact() {
-	Person person=new Person();
-	Scanner sc=new Scanner(System.in);
-	
-	System.out.println("Enter Firstname: ");
-	String firstname=sc.nextLine();
-	person.setfirstname(firstname);
-	
-	System.out.println("Enter Lastname: ");
-	String lastname=sc.nextLine();
-	person.setlastname(lastname);
-	
-	System.out.println("Enter Address: ");
-	String Address=sc.nextLine();
-	person.setStreet(Address);
 
-	
-	System.out.println("Enter City: ");
-	String City=sc.nextLine();
-	person.setCity(City);
-	
-	System.out.println("Enter State: ");
-	String State=sc.nextLine();
-	person.setState(State);
-	
-	System.out.println("Enter country: ");
-	String country=sc.nextLine();
-	person.setCountry(country);
-	
-	System.out.println("Enter Zipcode: ");
-	String Zipcode=sc.nextLine();
-	person.setZip(Zipcode);
-	
-	System.out.println("Enter Phonenumber: ");
-	String Phonenumber=sc.nextLine();
-	person.setPhone(Phonenumber);
-	
-	System.out.println("Enter Email: ");
-	String Email=sc.nextLine();
-	person.setEmail(Email);
-	
-	return person;
-	
-}
 
 public int searchExistingContact(String search_pers)
 {
@@ -96,33 +52,31 @@ public int searchExistingContact(String search_pers)
     return indx;
 }
 
-public void editExistingContact(){
+public void deleteExistingContact(){
     System.out.println("Enter the name of the person whose details you "
-            + "want to be changed");
+            + "want to delete");
     Scanner sc = new Scanner(System.in);
     String search_pers = sc.next();
     int index = searchExistingContact(search_pers);
-    if(index != -1 )
+    if(index != -1)
     {
-	    System.out.println("Found the name, Kindly enter new details ");
+	    System.out.println("Person Name is found");
 	    // Ask for the new details
-	    Person p = createcontact();
-	    personList.set(index, p);
+	    Person p = personList.remove(index);
+	    System.out.println(p.getfirstname()+" is deleted successfully.");
     }
     else
     {
-    	System.out.println("Person not found.");
+    	System.out.println(" Person not found ");
     }
 }
 
 	public static void main(String[] args) {
 		AddressBookSystem obj = new AddressBookSystem();
 		obj.setPersonDetails();
-		obj.editExistingContact();
+		obj.deleteExistingContact();
 		obj.displayPersonDetails();
-
 	}
-
 }
 
 
