d3a8d8a524933b0d572f352c9913ebd9f640ce9c
pranjalikhadatkar <94820061+pranjalikhadatkar@users.noreply.github.com>
2022-01-17 15:58:58 +0530

    Update AddressBookSystem.java

--- AddressBookSystem.java
+++ AddressBookSystem.java
@@ -1,9 +1,8 @@
-package com.AddressBookSystem;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class AddressBookSystem {
-	ArrayList<Person> personList = new ArrayList<Person>();
+public class UC4 {
+	public static ArrayList<Person> personList = new ArrayList<Person>();
 	public void displayPersonDetails()
 	{
 		for(Person p : personList)
@@ -21,18 +20,39 @@ public class AddressBookSystem {
 		}
 	}
 	
-public void setPersonDetails() 
-{
-		Person person1 = new Person("Pranjali", "khadatkar", "besa", "Nagpur", "Maharastra", "India", "8421078744","440034", "pranjalikhadatkar@gmail.com");
-		personList.add(person1);
-		Person person2 = new Person("pranay", "khadatkar", "besa", "Nagpur", "Maharastra", "India", "9012197036","440027", "khadatkarpranay@gmail.com");
-		personList.add(person2);
-		Person person3 = new Person("vishal", "pothare", "Pimpalgaon", "Hinganghat", "Maharastra", "India", "9637416031","442301", "potharevishal@gmail.com");
-		personList.add(person3);
-		Person person4 = new Person("kajal", "palange", "wardha", "wardha", "Maharastra", "India", "9430254160","440021", "kajalpalange@gmail.com");
-		personList.add(person4);
+public Person createcontact() {
+	Person person=new Person();
+	Scanner sc=new Scanner(System.in);
+	System.out.println("Enter Firstname: ");
+	String firstname=sc.nextLine();
+	person.setfirstname(firstname);
+	System.out.println("Enter Lastname: ");
+	String lastname=sc.nextLine();
+	person.setlastname(lastname);
+	System.out.println("Enter Address: ");
+	String Address=sc.nextLine();
+	person.setStreet(Address);	
+	System.out.println("Enter City: ");
+	String City=sc.nextLine();
+	person.setCity(City);
+	System.out.println("Enter State: ");
+	String State=sc.nextLine();
+	person.setState(State);
+	System.out.println("Enter country: ");
+	String country=sc.nextLine();
+	person.setCountry(country);	
+	System.out.println("Enter Zipcode: ");
+	String Zipcode=sc.nextLine();
+	person.setZip(Zipcode);
+	System.out.println("Enter Phonenumber: ");
+	String Phonenumber=sc.nextLine();
+	person.setPhone(Phonenumber);
+	System.out.println("Enter Email: ");
+	String Email=sc.nextLine();
+	person.setEmail(Email);
+	
+	return person;
 }
-
 
 public int searchExistingContact(String search_pers)
 {
@@ -72,11 +92,40 @@ public void deleteExistingContact(){
 }
 
 	public static void main(String[] args) {
-		AddressBookSystem obj = new AddressBookSystem();
-		obj.setPersonDetails();
-		obj.deleteExistingContact();
-		obj.displayPersonDetails();
+		UC4 obj = new UC4();
+		char ch='y'; 
+		int i = 1;
+		do
+		{
+			System.out.println("Enter your choice ");
+			System.out.println("1. Add Contact \n2. Delete Contact \n3. Display contacts \n4. Exit");
+			Scanner sc=new Scanner(System.in);
+			int choice=sc.nextInt();
+			switch(choice) {
+			case 1:
+				do
+				{
+					Person per=obj.createcontact();
+					personList.add(per);
+					System.out.println("Do you want to add another person if yes press y otherwise press any key");
+					ch=sc.next().charAt(0);
+				}while(ch=='y' || ch=='Y');
+				
+				break;
+			case 2:
+				obj.deleteExistingContact();
+				break;
+			case 3:
+				obj.displayPersonDetails();
+				break;
+			case 4:
+				System.exit(0);
+				System.out.println("End of the program");
+				break;
+			default:
+				System.out.println("You entered wrong choice");
+			}
+		
+		}while(i==1);
 	}
 }
-
-
